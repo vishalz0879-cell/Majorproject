a5eb4adbe5ee40121263a9af164bd58fcfb8192b
vishalz0879-cell <vishalz0879@gmail.com>
2026-09-02 09:56:49 +0530

    Created a authorization pathway and Post-login page so it will easier for user to come back agin to that page after login

--- init/index.js
+++ init/index.js
@@ -16,6 +16,7 @@ async function main() {
 
 const initDB= async()=>{
     await Listing.deleteMany({});
+    initdata.data=initdata.data.map((obj)=>({...obj,owner: '6a9520619a98235d1f176c8d'}));
     await Listing.insertMany(initdata.data)
     console.log("data was initalized")
 };
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router=express.Router();
 const User=require("../models/user.js");
 const wrapAsyc = require("../utils/wrapAsyc");
 const passport=require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs")
@@ -14,8 +15,13 @@ router.post("/signup",wrapAsyc( async(req,res)=>{
     const newUser= new User({email,username});
     const registeredUser = await User.register(newUser,password);
     console.log(registeredUser);
-    req.flash("success","Welcome to Wanderlust");
-    res.redirect("/listings")
+    req.login(registeredUser,(err)=>{
+         if(err){
+           return next(err);
+        }
+        req.flash("success","Welcome to wanderlust!")
+        res.redirect("/listings");
+    })
     }catch(e){
         req.flash("error", e.message);
         res.redirect("/signup");
@@ -29,13 +35,15 @@ router.get("/login",(req,res)=>{
 });
 
 router.post("/login", 
+    saveRedirectUrl,
      passport.authenticate('local', { 
     failureRedirect: '/login',
     failureFlash:true ,
     }),
     async(req,res)=>{
     req.flash("success","welcome back to wanderlust ! you are logged in!")
-    res.redirect("/listings");
+    let redirectUrl=res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
 })
 
 router.get("/logout",(req,res)=>{
