e6b3a24db4c9df00a3fafa5bdf39728cefd5a28f
vishalz0879-cell <vishalz0879@gmail.com>
2026-09-03 15:43:54 +0530

    added the ownership to every listing and also for the listings which are going to list

--- models/listing.js
+++ models/listing.js
@@ -30,7 +30,7 @@ const listingSchema = new Schema({
     ],
     owner:{
       type:Schema.Types.ObjectId,
-      ref:"user",
+      ref:"User",
     }
 
 });
--- routes/listings.js
+++ routes/listings.js
@@ -34,17 +34,19 @@ router.get("/new",isLoggedIn,(req,res)=>{
 //show route
 router.get("/:id",WrapAsync( async (req,res)=>{
     let {id}=req.params;
-    const listing =await Listing.findById(id).populate("reviews");
+    const listing =await Listing.findById(id).populate("reviews").populate("owner");
      if (!listing) {
         req.flash("error", "Listing you requested for does not exist!");
         return res.redirect("/listings");
      }
+     console.log(listing)
     res.render("listings/show.ejs",{listing})
 }))
 
 //create route
 router.post("/", isLoggedIn,validateListing, WrapAsync(async(req,res,next)=>{
     const newListing = new Listing(req.body.listing);
+    newListing.owner=req.user._id;
     await newListing.save();
     req.flash("success","New listing Created")
     res.redirect("/listings")
